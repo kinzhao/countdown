853545bfb90a7d4918fdeb20121e8cb895fd838a
kzhao <kin.zhao@mlb.com>
2017-06-20 00:06:24 -0400

    countdown setup

---
+++ scripts/site.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    var date = new Date( 2017, 8, 9 );
+    var now = new Date();
+    var diff = ( date.getTime() / 1000 ) - ( now.getTime() / 1000 );
+
+    var clock = $( '.countdown' ).FlipClock( diff, {
+        clockFace : 'DailyCounter',
+        countdown : true,
+        showSeconds: true
+    });
+});
+
+
