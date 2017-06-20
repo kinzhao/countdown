62a65fc2db24c59d854fff644dc817b5e6f28d07
Kin Z <developer@kinzhao.com>
2017-06-20 00:25:51 -0400

    Use ISO Date

--- scripts/site.js
+++ scripts/site.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    var date = new Date( 2017, 8, 9 );
+    var date = new Date( 2017-08-09 );
     var now = new Date();
     var diff = ( date.getTime() / 1000 ) - ( now.getTime() / 1000 );
 
