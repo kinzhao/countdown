76d2bd534865f2144b6761ace69e6f40a85823b3
kzhao <kin.zhao@mlb.com>
2017-06-20 16:39:49 -0400

    timezone update

--- scripts/site.js
+++ scripts/site.js
@@ -1,13 +1,15 @@
 $( document ).ready(function() {
-    var date = new Date( "2017-08-09" );
-    var now = new Date();
-    var diff = ( date.getTime() / 1000 ) - ( now.getTime() / 1000 );
+    var tz    = moment.tz.guess();
+    var date  = moment.tz( '2017-08-09', tz );
+    var now   = new Date();
+    var diff  = ( date.valueOf() / 1000 ) - ( now.getTime() / 1000 );
 
     var clock = $( '.countdown' ).FlipClock( diff, {
-        clockFace : 'DailyCounter',
-        countdown : true,
-        showSeconds: true
+        clockFace   : 'DailyCounter',
+        countdown   : true,
+        showSeconds : true
     });
+
 });
 
 
